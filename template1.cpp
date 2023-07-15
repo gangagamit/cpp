826d773718270ad63a686918edf25ff4d3df2a8a
gangagamit <gangagamit350@gmail.com>
2023-07-15 08:27:43 +0530

    15-7-2023

--- template1.cpp
+++ template1.cpp
@@ -5,9 +5,13 @@ void display(T t1)
 {
     cout<<"display template:"<<t1<<endl;
 }
-
+template<class x,class y>
+void Add(x a,y b)
+{
+    cout<<"Addition template:"<<a+b<<endl;
+}
 int main()
 {
     display(10);
-    
+    Add(78.45,89);
 }
